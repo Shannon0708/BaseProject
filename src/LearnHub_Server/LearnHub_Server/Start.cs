6e8fd2cb16c019d27c86e208b38026163157041e
Waylon <1664940243LWY@gmail.com>
2019-11-24 01:01:14 +0800

    版本號 v1.0.03

--- src/LearnHub_Server/LearnHub_Server/Start.cs
+++ src/LearnHub_Server/LearnHub_Server/Start.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-//版本1.0.01
+//版本1.0.03
 namespace LearnHub_Server {
 
     /// <summary>
@@ -33,7 +33,6 @@ namespace LearnHub_Server {
         /// </summary>
         private static void Initial() {
             Server = new Server();
-
         }
 
     }
