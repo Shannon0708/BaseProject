ce52ceffc62297670a588cd22b27c1c198324e52
Waylon <1664940243LWY@gmail.com>
2019-11-23 11:40:30 +0800

    Update Send()
    完善格式及內容

--- src/Packages/Network/Network/Packet.cs
+++ src/Packages/Network/Network/Packet.cs
@@ -10,24 +10,24 @@ namespace Network.Packet {
         /// </summary>
         /// <param name="crcCode">封包驗證碼</param>
         /// <param name="packageType">封包型態</param>
-        /// <param name="encryption">定義加密的方法</param>
-        /// <param name="Data_Byte">封包資料主體內容</param>
+        /// <param name="encryptionType">定義加密的方法</param>
+        /// <param name="data_Byte">封包資料主體內容</param>
         /// <returns>回傳封裝完畢的封包</returns>
-        private byte[] BuildPackage(int crcCode, PackageType packageType, Encryption encryption, byte[] Data_Byte) {
+        private byte[] BuildPackage(int crcCode, PackageType packageType, EncryptionType encryptionType, byte[] data_Byte) {
             
             //using System有:BitConverter GetBytes
             //using System.Net有:IPAddress HostToNetworkOrder把後面括號內的參數轉換成網路格式
             byte[] crcCode_Byte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(crcCode));             
-            byte[] packageType_Byte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)packageType));     //(short)強制將後面參數轉成短整數
-            byte[] encryption_Byte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)encryption));       
-            byte[] bodyLength = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(Data_Byte.Length));
+            byte[] packageType_Byte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)packageType));          //(short)強制將後面參數轉成短整數
+            byte[] encryptionType_Byte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)encryptionType));       
+            byte[] bodyLength = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(data_Byte.Length));
 
-            byte[] Packet = new byte[8 + Data_Byte.Length];
+            byte[] Packet = new byte[8 + data_Byte.Length];
 
             //CopyTo:把前面的內容複製到後面陣列的起始位置(陣列,起始位置)
             crcCode_Byte.CopyTo(Packet, 0);
             packageType_Byte.CopyTo(Packet, 4);
-            encryption_Byte.CopyTo(Packet, 6);
+            encryptionType_Byte.CopyTo(Packet, 6);
             bodyLength.CopyTo(Packet, 8);
 
             return Packet;
--- src/Packages/Network/Network/Temp.cs
+++ src/Packages/Network/Network/Temp.cs
@@ -2,16 +2,26 @@
 
 namespace Network.Packet {
 
+    /// <summary>
+    /// 封包型態
+    /// </summary>
     public enum PackageType {
         None,
         Test
     }
 
+    /// <summary>
+    /// 加密方法
+    /// </summary>
     public enum EncryptionType {
         None,
         RES256
     }
-    
+
+
+    /// <summary>
+    /// 用戶資料列表
+    /// </summary>
 	public class User {
     
     
